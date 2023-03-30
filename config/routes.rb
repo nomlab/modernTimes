804c68fe5aa402b78a247c298950b4b758ecc7e2
Yoshinari Nomura <nom@nomlab.org>
2023-03-30 17:14:29 +0900

    Remove shifts from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   resources :shift_types
   resources :assignments
   resources :nurses
-  resources :shifts
   resources :teams
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "shifts#index"
+  root "assignments#index"
 end
