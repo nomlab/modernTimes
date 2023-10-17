1f5dce861f8458b7d247a5494eae0d20d5f3ab6c
ueno12345 <ueno2022@s.okayama-u.ac.jp>
2023-10-17 15:14:47 +0900

    Wip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,20 @@
-//= require jquery3
 //= require popper
 //= require bootstrap
+//= require blockly/dist/blockly_compressed
+//= require blockly/dist/blocks_compressed
+//= require blockly/dist/javascript_compressed
+//= require blockly/msg/messages
+
+document.addEventListener("DOMContentLoaded", function() {
+    var toolbox = '<xml>Your toolbox content here</xml>';
+  
+    var workspace = Blockly.inject('blocklyDiv', {
+      toolbox: toolbox,
+      // その他の設定をここに追加できます
+    });
+  
+    // ブロックが変更されたときの処理を追加（必要に応じて）
+    workspace.addChangeListener(function(event) {
+      // 変更があったときの処理をここに追加
+    });
+  });  
