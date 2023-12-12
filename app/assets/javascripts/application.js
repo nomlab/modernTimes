2a8e69b12d5e575888d0c397a21e497c27a9211c
ueno12345 <ueno2022@s.okayama-u.ac.jp>
2023-12-12 22:02:39 +0900

    Update table

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,7 @@
+//= require_tree .
 //= require popper
 //= require bootstrap
-
+//= require blockly/dist/blockly_compressed
+//= require blockly/dist/blocks_compressed
+//= require blockly/dist/javascript_compressed
+//= require blockly/msg/messages
