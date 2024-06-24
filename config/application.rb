5f52031bb76dba5d04d0cf2bff9744edac2ded31
ueno12345 <ueno2022@s.okayama-u.ac.jp>
2024-06-24 14:06:17 +0900

    Set locale

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module ModernTimes
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.time_zone = 'Asia/Tokyo'
+
+    config.i18n.default_locale = :ja
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
