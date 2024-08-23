1908eba14f304e55b98d01e03318541afc07a93b
ueno12345 <ueno2022@s.okayama-u.ac.jp>
2024-08-23 11:23:56 +0900

    Separete table

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -153,7 +153,7 @@ class AssignmentsController < ApplicationController
         nurse.domain.update(timeslots.uniq, :timeslots)
       end
     else
-      redirect_to assignments_path(month: "202406"), notice: "シフトを作成できませんでした．\n制約条件を変更してください．", flash: {color: :red}
+      redirect_to assignments_path(month: "202409"), notice: "シフトを作成できませんでした．\n制約条件を変更してください．", flash: {color: :red}
       return
     end
     #shift_jsonの形式 {"name"=>"nurse 5", "date"=>"2024-03-01", "shifttype"=>"日勤"}
@@ -161,7 +161,7 @@ class AssignmentsController < ApplicationController
     create_assignments(shift_json)
 #    @html = ast.to_html
 #"    redirect_to solve_path(result: @html)
-    redirect_to assignments_path(month: "202406"), notice: "シフトを作成しました．\n制約条件は成立可能です．", flash: {color: :green}
+    redirect_to assignments_path(month: "202409"), notice: "シフトを作成しました．\n制約条件は成立可能です．", flash: {color: :green}
   end
 
   def download
