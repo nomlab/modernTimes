38244896a6b6c75de20d0ae8997df57759ae818d
ueno12345 <ueno2022@s.okayama-u.ac.jp>
2024-08-27 11:17:00 +0900

    Update nurse's show

--- app/controllers/rails_nurses_controller.rb
+++ app/controllers/rails_nurses_controller.rb
@@ -24,6 +24,9 @@ class RailsNursesController < ApplicationController
       @month = Date.new(today.year, today.month, 1)
     end
     @assignments = Assignment.where(rails_nurse_id: @nurse.id, date: @month...(@month >> 1))
+
+    @shift_names = ShiftType.distinct.pluck(:name)
+    @shift_names += ["日勤", "準夜勤", "深夜勤", "休み"] if @shift_names.size < 4
     @shift_types = ShiftType.all
     @shift_type_counts = {}
     @total = 0
@@ -31,6 +34,11 @@ class RailsNursesController < ApplicationController
       @shift_type_counts[shift_type.name] = count_shifts_by_shift_type(shift_type)
       @total += @shift_type_counts[shift_type.name]
     end
+
+    @nurse_shift_counts = count_shifts_by_nurse
+    @daily_shift_counts = count_shifts_by_day
+
+    @date_range = @month.to_date...(@month >> 1).to_date
   end
 
   # GET /nurses/new
@@ -95,5 +103,30 @@ class RailsNursesController < ApplicationController
 
     def count_shifts_by_shift_type(shift_type)
       @assignments.where(shift_type_id: shift_type.id).count
+    end
+
+    def count_shifts_by_nurse
+      nurse_shift_counts = {}
+      @assignments.group_by(&:rails_nurse_id).each do |nurse_id, nurse_assignments|
+        shift_counts = {}
+        @shift_types.each do |shift_type|
+          shift_counts[shift_type.name] = nurse_assignments.count { |a| a.shift_type_id == shift_type.id }
+        end
+        nurse_shift_counts[nurse_id] = shift_counts
+      end
+      nurse_shift_counts
+    end
+
+    def count_shifts_by_day
+      daily_shift_counts = {}
+      (@month...(@month >> 1)).each do |day|
+        daily_shifts = @assignments.where(date: day)
+        shift_counts = {}
+        @shift_types.each do |shift_type|
+          shift_counts[shift_type.name] = daily_shifts.where(shift_type_id: shift_type.id).count
+        end
+        daily_shift_counts[day] = shift_counts
+      end
+      daily_shift_counts
     end
 end
